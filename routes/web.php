6cf3240da304b389a7da1b4fd90fe646c8ae6268
Wahyu-Harta-Sasmita <wahyusasmitaharta561@gmail.com>
2024-12-28 10:59:01 +0800

    new

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,12 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// USER
+Route::middleware('guest')->group(function () {
+
+});
+
+// ADMIN
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
